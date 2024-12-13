c7a8520ab0258ff79ccb685904a47ea1788e3a96
dkhawk <107309+dkhawk@users.noreply.github.com>
2024-12-13 08:23:59 -0700

    Day 13.  Part 2

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -25,35 +25,66 @@ val testInput = """
 val buttonACost = 3
 val buttonBCost = 1
 
+val costBasis = VectorLong(buttonACost.toLong(), buttonBCost.toLong())
+
 data class Machine(
-    val btnA: Vector,
-    val btnB: Vector,
-    val prize: Vector,
+    val btnA: VectorLong,
+    val btnB: VectorLong,
+    val prize: VectorLong,
 )
 
 fun main() {
-    val machines = parseInput(testInput)
+    val testMachines = parseInput(testInput)
+    val realMachines = parseInput(readLines("inputs/13"))
 
-    check(part1(machines) == 480)
-//    check(part2(machines) == 15)
-
-    val machinesReal = parseInput(readLines("inputs/13"))
+    check(part1(testMachines) == 480L)
     "25751 is correct!!".println()
-    part1(machinesReal).println()
-//    part2(machinesReal).println()
+    part1(realMachines).println()
+
+    "108496818472493 is too low!".println()
+    "108528956728655 is correct!!".println()
+    part2(realMachines).println()
 }
 
-private fun part1(machines: List<Machine>): Int {
+private fun Machine.fixUnitError(): Machine {
+    val newPrize = VectorLong(prize.x + 10000000000000L, prize.y + 10000000000000L)
+    return this.copy(prize = newPrize)
+}
+
+private fun Machine.solveLong(limit: Long? = null): Long? {
+    val (i, j) = btnA
+    val (k, l) = btnB
+    val (m, n) = prize
+
+    val aPresses = ((k * n) - (m * l)) / ((k * j) - (l * i))
+    val bPresses = (m - (aPresses * i)) / k
+
+    if (limit != null) {
+        if (aPresses > limit || bPresses > limit) {
+            return null
+        }
+    }
+
+    return if ((btnA * aPresses) + (btnB * bPresses) == prize) {
+        VectorLong(aPresses, bPresses).entrywiseProduct(costBasis).sum()
+    } else {
+        null
+    }
+}
+
+private fun part1(machines: List<Machine>): Long {
     return machines.mapNotNull { machine ->
-        playMachine(machine)
-    }.sum()
+        //  playMachine(machine)
+        machine.solveLong(100)
+    }.fold(0L) { a, b -> a + b }
 }
 
-fun playMachine(machine: Machine): Int? {
-    val maxBPresses = max(100,
+// Original part 1 solution
+fun playMachine(machine: Machine): Long? {
+    val maxBPresses = max(100L,
         min(machine.prize.x / machine.btnB.x, machine.prize.y / machine.btnB.y))
 
-    return (0..maxBPresses).firstOrNull { bPresses ->
+    return (0L..maxBPresses).firstOrNull { bPresses ->
         val target = machine.prize - (machine.btnB * bPresses)
         val aPresses = (target.x / machine.btnA.x)
 
@@ -67,15 +98,18 @@ fun playMachine(machine: Machine): Int? {
         check(aPresses <= 100)
         check(bPresses <= 100)
 
-        check(target.x.rem(machine.btnA.x) == 0)
-        check(target.y.rem(machine.btnA.y) == 0)
+        check(target.x.rem(machine.btnA.x) == 0L)
+        check(target.y.rem(machine.btnA.y) == 0L)
 
-        (target.x / machine.btnA.x) * buttonACost + (bPresses * buttonBCost)
+        VectorLong(target.x / machine.btnA.x, bPresses).entrywiseProduct(costBasis).sum()
+//        (target.x / machine.btnA.x) * buttonACost + (bPresses * buttonBCost)
     }
 }
 
-private fun part2(input: List<Machine>): Int {
-    return -1
+private fun part2(machines: List<Machine>): Long {
+    return machines.mapNotNull { machine ->
+        machine.fixUnitError().solveLong()
+    }.fold(0) { a, b -> a + b }
 }
 
 private fun parseInput(input: List<String>): List<Machine> {
@@ -98,8 +132,8 @@ private fun parseMachine(lines: List<String>): Machine {
 
 val buttonRegex = """X[+=](?<x>[0-9]*), Y[+=](?<y>[0-9]*)""".trimMargin().toRegex()
 
-private fun parseMachineLine(line: String): Vector {
+private fun parseMachineLine(line: String): VectorLong {
     return buttonRegex.find(line)!!.groups.let { groups ->
-        Vector(groups["x"]!!.value.toInt(), groups["y"]!!.value.toInt())
+        VectorLong(groups["x"]!!.value.toLong(), groups["y"]!!.value.toLong())
     }
 }
--- src/utils/Vector.kt
+++ src/utils/Vector.kt
@@ -26,3 +26,38 @@ data class Vector(val x: Int = 0, val y: Int = 0) {
         return Vector(x * scalar, y * scalar)
     }
 }
+
+data class VectorLong(val x: Long, val y: Long) {
+    fun advance(heading8: Heading8): VectorLong {
+        return this + heading8.vector.toLongVector()
+    }
+
+    fun advance(heading: Heading): VectorLong {
+        return this + heading.vector.toLongVector()
+    }
+
+    operator fun unaryMinus(): VectorLong = VectorLong(-x, -y)
+    operator fun plus(other: VectorLong): VectorLong {
+        return VectorLong(x + other.x, y + other.y)
+    }
+
+    operator fun minus(other: VectorLong): VectorLong {
+        return VectorLong(x - other.x, y - other.y)
+    }
+
+    operator fun plus(heading8: Heading8): VectorLong {
+        return VectorLong(x + heading8.vector.x, y + heading8.vector.y)
+    }
+
+    operator fun times(scalar: Long): VectorLong {
+        return VectorLong(x * scalar, y * scalar)
+    }
+
+    fun entrywiseProduct(costBasis: VectorLong): VectorLong {
+        return VectorLong(x = x * costBasis.x, y = y * costBasis.y)
+    }
+
+    fun sum() = x + y
+}
+
+private fun Vector.toLongVector() = VectorLong(x.toLong(), y.toLong())
