e7d48e77e683af2de7a738acf17a981d3dffc2e5
dkhawk <107309+dkhawk@users.noreply.github.com>
2024-12-15 08:29:57 -0700

    Base case to bootstrap each day

---
+++ src/day16/Day16.kt
@@ -0,0 +1,22 @@
+package day16
+
+import utils.*
+
+val testInput = """
+    
+""".trimIndent().lines()
+
+fun main() {
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readLines("inputs/0")
+}
+
+private fun part1(input: List<String>): Int {
+    return -1
+}
+
+private fun part2(input: List<String>): Int {
+    return -1
+}
