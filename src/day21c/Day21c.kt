06c8861abe4d8fef89f2681fe300a98935e124f7
dkhawk <107309+dkhawk@users.noreply.github.com>
2024-12-26 08:30:28 -0700

    Day 21. Trying again.

--- src/day21c/Day21c.kt
+++ src/day21c/Day21c.kt
@@ -53,6 +53,10 @@ sealed interface Robot {
             l = l.advance(heading)
         }
     }
+
+    fun pathToLocations(goalPath: String): List<Vector> {
+        return goalPath.map { symbol -> pad[symbol]!! }
+    }
 }
 
 data class DpadRobot(override val location: Vector = dpad['A']!!) : Robot {
@@ -72,37 +76,200 @@ data class KeypadRobot(override val location: Vector = keypad['A']!!) : Robot {
 }
 
 private fun part1(input: List<String>): Int {
-    val keypadRobot = KeypadRobot()
-    val keypadPaths = getAllPaths(keypadRobot, '0')
-    keypadPaths.toList().printAsLines()
-    val keypadMoves = keypadPaths.map { path ->
-        keypadRobot.pathToGoalString(path)
-    }.toList()
+    val robots = listOf(
+        KeypadRobot()
+    )
 
-    keypadMoves.map { moveSequence ->
-        val radiationRobot = DpadRobot()
-        val radiationPaths = getAllPaths(radiationRobot, moveSequence)
+    val goalPath = "029A"
 
-        val radiationMoves = radiationPaths.map { paths ->
-            paths.toList().map {
-                radiationRobot.pathToGoalString(it)
-            }
+//    val paths = getAllPaths(robots.first(), Vector(1,2), Vector(2, 0))
+//
+//    paths.printAsLines()
+//
+//    TODO()
+
+    val shortestPath = getShortestPath(robots, goalPath)
+
+    shortestPath.println()
+
+//
+//
+//    val keypadRobot = KeypadRobot()
+//    val keypadPaths = getAllPaths(keypadRobot, '0')
+//
+//    "Keypad moves".println()
+//    keypadPaths.toList().printAsLines()
+//    println()
+//
+//    val keypadMoves = keypadPaths.map { path ->
+//        keypadRobot.pathToGoalString(path)
+//    }.toList()
+//
+//
+//    keypadMoves.map { moveSequence ->
+//        val radiationRobot = DpadRobot()
+//        val radiationPaths = getAllPaths(radiationRobot, moveSequence)
+//
+//        val radiationMoves = radiationPaths.map { paths ->
+//            paths.toList().map {
+//                radiationRobot.pathToGoalString(it)
+//            }
+//        }
+//
+//        "Radiation moves".println()
+//        radiationMoves.printAsLines()
+//        println()
+//
+//        radiationMoves.map { radiationMoveList ->
+//            "Next radiation path".println()
+//            radiationMoveList.println()
+//
+//             radiationMoveList.map {moves ->
+//                val coldRobot = DpadRobot()
+//                val paths = getAllPaths(coldRobot, moves).map { candidate ->
+//                    candidate.map { coldRobot.pathToGoalString(it).toList() }.toList()
+//                }
+//                paths.printAsLines()
+//                paths
+//            }
+//        }
+//    }
+
+    TODO()
+}
+
+private fun getShortestPath(robots: List<KeypadRobot>, outputToProduce: String): String {
+    if (robots.isEmpty()) {
+        // For example, if I were able ot stand directly at the keypad, this would be the solution
+        return outputToProduce
+    }
+
+    val bot = robots.first()
+
+    // Turn the goalPath into a series of locations
+    val locationsToVisit = ArrayDeque(bot.pathToLocations(outputToProduce))
+
+    locationsToVisit.addFirst(bot.location)
+
+    val subGoals = locationsToVisit.windowed(2, 1) {(src, dst) ->
+        src to dst
+    }
+
+    val problemsToSolve = subGoals.map { subGoal ->
+        println("")
+        val src = subGoal.first
+        val dst = subGoal.second
+        println("Go from $src to $dst (${bot.grid[src]} to ${bot.grid[dst]})")
+        val possiblePaths = getAllPaths(bot, subGoal.first, subGoal.second)
+        possiblePaths.printAsLines()
+        println("")
+        possiblePaths
+    }
+
+    problemsToSolve.map { subPathOptions ->
+        // We have a list of all paths to consider between some src and dst
+        val movementOptions = subPathOptions.map { pathAsLocations ->
+            // Translate this to a dpad movement path
+            // Don't forget to push the button
+            pathAsLocations.toDpadSequence() + 'A'
         }
 
-        radiationMoves.printAsLines()
+        // Now, which of these is going to give us the shortest result?
 
-        radiationMoves.forEach { candidate ->
-            radiationMoves.map { moveSequence ->
-                val coldRobot = DpadRobot()
-//                val coldPaths = getAllPaths(coldRobot, moveSequence)
+        val shortest = movementOptions.map { path ->
+            getShortestPath(robots.drop(1), path)
+        }.minBy { it.length }
 
-            }
-        }
+        shortest.println()
 
+        println("")
     }
 
     TODO()
 }
+
+private fun List<Vector>.toDpadSequence(): String {
+    return windowed(2, 1).map { (a, b) ->
+        b - a
+    }.map { it.toHeading().toSymbol() }.joinToString("")
+}
+
+// Include the source and the destination!
+private fun getAllPaths(
+    bot: Robot,
+    src: Vector,
+    dst: Vector
+): List<List<Vector>> {
+    val allHeadingSequences = getHeadingSequences(src, dst).toList() //.also { .printAsLines() }
+
+    val permutations = allHeadingSequences.permutations().toList()
+
+    val movementSequences = permutations.map { headings ->
+        var location = src
+
+        buildList {
+            add(src)
+            headings.forEach { heading ->
+                location.advance(heading).also {
+                    location = it
+                    add(it)
+                }
+            }
+        }
+    }
+
+    val legalPaths = movementSequences.filter { locations ->
+        locations.all {
+            val isLegal = bot.isLegalLocation(it)
+            isLegal
+        }
+    }
+
+    return legalPaths
+}
+
+
+private fun getHeadingSequences(src: Vector, dst: Vector) = sequence {
+    val delta = dst - src
+
+    if (delta.x > 0) {
+        repeat(delta.x) {
+            yield(Heading.EAST)
+        }
+    }
+
+    if (delta.x < 0) {
+        repeat(-delta.x) {
+            yield(Heading.WEST)
+        }
+    }
+
+    if (delta.y > 0) {
+        repeat(delta.y) {
+            yield(Heading.SOUTH)
+        }
+    }
+
+    if (delta.y < 0) {
+        repeat(-delta.y) {
+            yield(Heading.NORTH)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 private fun getAllPaths(
     bot: Robot,
